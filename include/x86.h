5e3f914d127f4645013ca620c87a475efbc0da26
Michael Clark <michaeljclark@mac.com>
2025-01-10 09:00:03 +1300

    includes: minimize unnecessary includes in the main header

--- include/x86.h
+++ include/x86.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdlib.h>
+#include <stddef.h>
 #include <string.h>
 
 #include "types.h"
--- lib/x86.c
+++ lib/x86.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <limits.h>
 
 #include "x86.h"
